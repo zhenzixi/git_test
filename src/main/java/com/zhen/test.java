bbd778da18f14f8d71911de66369f768bb9b886f
zhenzixi <zhenzixi2022@163.com>
2022-08-04 17:11:41 +0800

    second commit

--- src/main/java/com/zhen/test.java
+++ src/main/java/com/zhen/test.java
@@ -4,5 +4,7 @@ public class test {
 
     public static void main(String[] args) {
         System.out.println("git test add");
+        System.out.println("git test 222");
+        System.out.println("git test 222");
     }
 }
